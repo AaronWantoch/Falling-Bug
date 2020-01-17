d418379197fcde411cfbab40d82934336b35e3c2
credus <aaron.wantoch@gmail.com>
2020-01-17 20:44:13 +0100

    can play

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField] float dyingDelay = 3f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        StartCoroutine(Die());
+    }
+
+    IEnumerator Die()
+    {
+        //Play Death Particles and Sound
+
+        GetComponent<PlayerMovement>().enabled = false;
+        yield return new WaitForSeconds(dyingDelay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 50f;
+    [SerializeField] float fallSpeed = 50f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(
+            CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed * Time.deltaTime
+            , -fallSpeed * Time.deltaTime
+            ,CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed * Time.deltaTime);
+    }
+}
